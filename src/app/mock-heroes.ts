de42be25e8a507e4bf540d58e4405297f0d5fef0
Francio00 <francheisy655@gmail.com>
2018-07-02 19:19:19 +0200

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{id: 2, name : 'Mr.Mime'}
+	{id: 3, name : 'Pablo'}
+	{id: 4, name : 'Mr.2'}
+	{id: 5, name : 'Il Kris'}
+	{id: 6, name : 'Gimmi'}
+	{id: 7, name : "L'indiano di MCT"}
+	{id: 8, name : 'Mr.Mim2'}
+	{id: 9, name : 'Mr.Mim3'}	
+];
